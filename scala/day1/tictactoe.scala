7d94e9775a75e321902dec69b89c85a9a14a6559
Dan Fitch <dgfitch@gmail.com>
2011-12-05 23:43:29 -0600

    scala day1: working playable tic tac toe. yeesh.

--- scala/day1/tictactoe.scala
+++ scala/day1/tictactoe.scala
@@ -6,32 +6,69 @@ class Board() {
   // I forget who traditionally goes first
   var currentPlayer = 1
 
+  def playerString(x:Int) = {
+    if (x == 1) {
+      "X"
+    } else if (x == 2) {
+      "O"
+    } else {
+      "_"
+    }
+  }
+
+  def currentPlayerString() = playerString(currentPlayer)
+
   def isFull() = {
     !(state contains(0))
   }
 
-  def checkSlot(slot:(Int,Int,Int)) = {
-    slot match {
-      case (x,y,z) =>
+  def hasWinner() = {
+    !( winner().isEmpty )
+  }
+
+  def winner() : Option[String] = {
+    val flat = possibleWins flatMap { case (x,y,z) =>
         if (state(x) == state(y) && state(x) == state(z) && state(x) > 0) {
           Some(state(x)) 
         } else { 
           None
         } 
     }
-  }
 
-  def winner() : Option[Int] = {
-    // there has GOT to be a better way to pick 1 Some option from a mapped sequence
-    val wins = possibleWins.map { checkSlot } flatten;
-    if (wins.length > 0) {
-      Some(wins.head)
+    // there has STILL GOT to be a better way to pick 1 Some option from an iterable thing filled with Options
+    if (flat.length > 0) {
+      Some(playerString(flat(0)))
     } else {
       None
     }
   }
 
-  def playAt(thing:Int, index:Int) {
-    state(index) = thing
+
+  def playAt(index:Int) {
+    state(index) = currentPlayer
+    if (currentPlayer == 1) {
+      currentPlayer = 2
+    } else {
+      currentPlayer = 1
+    }
+  }
+
+  def print() {
+    printf("%s | %s | %s\n", playerString(state(0)), playerString(state(1)), playerString(state(2)))
+    printf("--+---+--\n")
+    printf("%s | %s | %s\n", playerString(state(3)), playerString(state(4)), playerString(state(5)))
+    printf("--+---+--\n")
+    printf("%s | %s | %s\n", playerString(state(6)), playerString(state(7)), playerString(state(8)))
   }
 }
+
+val b = new Board()
+while (!b.isFull() && !b.hasWinner()) {
+  b.print()
+  printf("Need a move from %s (0-indexed lolz):\n", b.currentPlayerString())
+  val line = readLine()
+  // don't care enough now to add error checking for playing in a spot already taken
+  b.playAt(line.toInt)
+}
+
+printf("THE WINNER IS %s\n", b.winner())

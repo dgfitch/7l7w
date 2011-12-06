d8b32aa058044049186eba35810c0f96aa219edc
Dan Fitch <dgfitch@gmail.com>
2011-12-05 23:09:29 -0600

    scala day 1: now I switched it to Int instead of a case class to store the slot states, not understanding why map ... flatten fails

--- scala/day1/tictactoe.scala
+++ scala/day1/tictactoe.scala
@@ -1,25 +1,19 @@
-abstract class Mark
-case class X() extends Mark
-case class O() extends Mark
-case class Blank() extends Mark
-val b = Blank()
-
 class Board() {
-  // Probably not the best way to do this
-  var state : Array[Mark] = Array(b,b,b,b,b,b,b,b,b) 
+  // state is either 0 (blank), 1 (X), or 2 (O) -- I couldn't get case classes to work???
+  var state = Array(0,0,0,0,0,0,0,0,0) 
   val possibleWins = Array((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,5,0))
 
   // I forget who traditionally goes first
-  var currentPlayer = X()
+  var currentPlayer = 1
 
   def isFull() = {
-    !(state contains(b))
+    !(state contains(0))
   }
 
   def checkSlot(slot:(Int,Int,Int)) = {
     slot match {
       case (x,y,z) =>
-        if (state(x) == state(y) && state(x) == state(z)) {
+        if (state(x) == state(y) && state(x) == state(z) && state(x) > 0) {
           Some(state(x)) 
         } else { 
           None
@@ -27,12 +21,17 @@ class Board() {
     }
   }
 
-  def winner() : Option[Mark] = {
-    val maybeWins = possibleWins.map { checkSlot }
-    maybeWins.flatten
+  def winner() : Option[Int] = {
+    // there has GOT to be a better way to pick 1 Some option from a mapped sequence
+    val wins = possibleWins.map { checkSlot } flatten;
+    if (wins.length > 0) {
+      Some(wins.head)
+    } else {
+      None
+    }
   }
 
-  def playAt(thing:Mark, index:Int) {
+  def playAt(thing:Int, index:Int) {
     state(index) = thing
   }
 }
